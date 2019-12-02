8a2af92696c71e3be1826436a57a7acd747d22ee
kangmin46 <kangmin789@naver.com>
2019-12-02 17:42:43 +0900

    refactor: update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,10 +1,8 @@
 sudo snap install docker --devmode
 sudo git clone -b practice https://github.com/EDD-season2/caffeine.git
-ls -al
 cd caffeine
-ls -al
 sudo git clone -b build https://github.com/kangmin46/dp.git
-ls -al
+sudo git clone -b develop https://github.com/edd-season2/caffeine-fe-shop.git
 sudo docker-compose up -d --build
 
 
