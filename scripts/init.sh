a14cc570a39840466c43c0e87b7005db45422862
kangmin46 <kangmin789@naver.com>
2019-12-03 17:40:13 +0900

    refactor: update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,14 @@
 sudo snap install docker --devmode
-sudo snap install npm -y 
+sudo apt-get install npm -y 
 
-sudo git clone -b practice https://github.com/EDD-season2/caffeine.git
+git clone -b practice https://github.com/EDD-season2/caffeine.git
 cd caffeine
-sudo git clone -b build https://github.com/kangmin46/dp.git
+git clone -b build https://github.com/kangmin46/dp.git
 
-sudo git clone -b develop https://github.com/edd-season2/caffeine-fe-shop.git
+git clone -b develop https://github.com/edd-season2/caffeine-fe-shop.git
 cd caffeine-fe-shop
-sudo npm install
-sudo npm run build
+npm install
+npm run build
 cd ..
 
 sudo docker-compose up -d --build
