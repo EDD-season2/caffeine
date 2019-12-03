83dc616d9b7b11c2e59db1e6fd10c71912f407a7
kangmin46 <kangmin789@naver.com>
2019-12-03 14:34:34 +0900

    refactor docker-compose.yml, init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,10 +1,14 @@
 sudo snap install docker --devmode
+sudo snap install npm -y 
+
 sudo git clone -b practice https://github.com/EDD-season2/caffeine.git
 cd caffeine
 sudo git clone -b build https://github.com/kangmin46/dp.git
+
 sudo git clone -b develop https://github.com/edd-season2/caffeine-fe-shop.git
+cd caffeine-fe-shop
+sudo npm build
+cd ..
+
 sudo docker-compose up -d --build
-
-
-
 
