e2aba27309d976ff2f20e1fc87bd0e3e3d2347f4
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-12-04 13:18:55 +0900

    refactor: update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,30 +3,34 @@
 sudo snap install docker --devmode
 sudo apt-get install npm -y 
 
-if [ ! -d caffeine ]; then
+CAFFEINE="caffeine/";
+BACK="dp/";
+FRONT="caffeine-fe-shop/";
+
+if [ ! -d "$CAFFEINE" ]; then
     ls -al
     git clone -b practice https://github.com/EDD-season2/caffeine.git
-    cd caffeine
+    cd "$CAFFEINE"
 else
-    cd caffeine
+    cd "$CAFFEINE"
     git pull
 fi
 
-if [ ! -d dp ]; then
+if [ ! -d "$BACK" ]; then
     ls -al
     git clone -b build https://github.com/kangmin46/dp.git
 else
-    cd dp
+    cd "$BACK"
     git pull
 fi
 
-if [ ! -d caffeine-fe-shop ]; then
+if [ ! -d "$FRONT" ]; then
     ls -al
     git clone -b develop https://github.com/edd-season2/caffeine-fe-shop.git
-    cd caffeine-fe-shop
+    cd "$FRONT"
     npm install
 else
-    cd caffeine-fe-shop
+    cd "$FRONT"
     git pull
 fi
 
